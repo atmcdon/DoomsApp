a5ea3f72f1d444589f7b0df85691bf6e9cdf4398
Celina G Schlecht <156964413+cgscgscgs@users.noreply.github.com>
2025-03-06 12:59:02 -0800

    asked for number

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*  date             developer              changes
+ *  3/6/2025         Celina                 Asked user input for number
+ * 
+ * 
+ */
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("This is a new app");
 
 Console.WriteLine("Change");
+
+Console.WriteLine("Bananas");
+
+//prompt user input
+Console.WriteLine("What's your favorite number?"); 
